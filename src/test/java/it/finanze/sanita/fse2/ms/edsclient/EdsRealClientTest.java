1207b45604bf15f955bcbe92ffe45f7526809660
Vincenzo Ingenito <Vincenzo.Ingenito-CIC@ibm.com>
2022-09-23 20:40:40 +0200

    feat(Test) : Increase coverage

--- src/test/java/it/finanze/sanita/fse2/ms/edsclient/EdsRealClientTest.java
+++ src/test/java/it/finanze/sanita/fse2/ms/edsclient/EdsRealClientTest.java
@@ -1,20 +1,35 @@
 package it.finanze.sanita.fse2.ms.edsclient;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 import java.util.UUID;
 
+import it.finanze.sanita.fse2.ms.edsclient.dto.response.DocumentResponseDTO;
+import it.finanze.sanita.fse2.ms.edsclient.dto.response.EDSPublicationResponseDTO;
+import it.finanze.sanita.fse2.ms.edsclient.dto.response.LogTraceInfoDTO;
+import it.finanze.sanita.fse2.ms.edsclient.exceptions.BusinessException;
+import it.finanze.sanita.fse2.ms.edsclient.exceptions.ConnectionRefusedException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.HttpServerErrorException;
 
 import it.finanze.sanita.fse2.ms.edsclient.config.Constants;
 import it.finanze.sanita.fse2.ms.edsclient.repository.entity.IniEdsInvocationETY;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "mock.disabled=true")
 @ComponentScan(basePackages = {Constants.ComponentScan.BASE})
@@ -24,14 +39,70 @@ class EdsRealClientTest extends AbstractTest {
     @Autowired
     MongoTemplate mongoTemplate;
 
+    @SpyBean
+    RestTemplate restTemplate;
+
     @Test
-    void publishTestWithRealClient() {
+    void publishErrorTestWithRealClient() {
 
         final String workflowInstanceId = UUID.randomUUID().toString();
         final IniEdsInvocationETY invocation = insertIniEdsInvocation(workflowInstanceId);
-
+        mockEdsClient(invocation, false, HttpStatus.INTERNAL_SERVER_ERROR);
         assertNotNull(invocation, "Invocation must exist to test the client call");
         assertThrows(HttpServerErrorException.InternalServerError.class, () -> callEdsClient(workflowInstanceId));
     }
 
+    @Test
+    void publishConnectionRefusedTestWithRealClient() {
+
+        final String workflowInstanceId = UUID.randomUUID().toString();
+        final IniEdsInvocationETY invocation = insertIniEdsInvocation(workflowInstanceId);
+
+        mockEdsClient(invocation, false, HttpStatus.REQUEST_TIMEOUT);
+        assertNotNull(invocation, "Invocation must exist to test the client call");
+        assertThrows(HttpServerErrorException.InternalServerError.class, () -> callEdsClient(workflowInstanceId));
+    }
+
+    @Test
+    void publishGenericExceptionTestWithRealClient() {
+        final String workflowInstanceId = UUID.randomUUID().toString();
+        final IniEdsInvocationETY invocation = insertIniEdsInvocation(workflowInstanceId);
+
+        mockEdsClient(invocation, false, HttpStatus.OK);
+        assertNotNull(invocation, "Invocation must exist to test the client call");
+        ResponseEntity<EDSPublicationResponseDTO> response = callEdsClient(workflowInstanceId);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        EDSPublicationResponseDTO mockResponse = new EDSPublicationResponseDTO();
+        mockResponse.setEsito(false);
+        assertEquals(mockResponse.getEsito(), response.getBody().getEsito());
+    }
+
+
+    @Test
+    void publishSuccessTestWithRealClient() {
+
+        final String workflowInstanceId = UUID.randomUUID().toString();
+        final IniEdsInvocationETY invocation = insertIniEdsInvocation(workflowInstanceId);
+
+        mockEdsClient(invocation, true, HttpStatus.OK);
+        assertNotNull(invocation, "Invocation must exist to test the client call");
+        ResponseEntity<EDSPublicationResponseDTO> response = callEdsClient(workflowInstanceId);
+        EDSPublicationResponseDTO mockResponse = new EDSPublicationResponseDTO();
+        mockResponse.setEsito(true);
+        assertEquals(mockResponse.getEsito(), response.getBody().getEsito());
+    }
+
+    private void mockEdsClient(final IniEdsInvocationETY invocation, final boolean outcome, HttpStatus status) {
+        LogTraceInfoDTO logTraceInfoDTO = new LogTraceInfoDTO("spanId", "traceId");
+        DocumentResponseDTO mockResponse = new DocumentResponseDTO(logTraceInfoDTO, "transactionId");
+        if (outcome) {
+            doReturn(new ResponseEntity<>(mockResponse, HttpStatus.OK)).when(restTemplate).exchange(anyString(), eq(HttpMethod.POST), any(HttpEntity.class), eq(DocumentResponseDTO.class));
+        } else if (status.is5xxServerError()) {
+            doThrow(HttpServerErrorException.InternalServerError.class).when(restTemplate).exchange(anyString(), eq(HttpMethod.POST), any(HttpEntity.class), eq(DocumentResponseDTO.class));
+        } else if (status.is4xxClientError()) {
+            doThrow(new ResourceAccessException("")).when(restTemplate).exchange(anyString(), eq(HttpMethod.POST), any(HttpEntity.class), eq(DocumentResponseDTO.class));
+        } else {
+            doReturn(new ResponseEntity<>(null, HttpStatus.BAD_REQUEST)).when(restTemplate).exchange(anyString(), eq(HttpMethod.POST), any(HttpEntity.class), eq(DocumentResponseDTO.class));
+        }
+    }
 }
---
+++ src/test/java/it/finanze/sanita/fse2/ms/edsclient/EnumsTest.java
@@ -0,0 +1,55 @@
+package it.finanze.sanita.fse2.ms.edsclient;
+
+import it.finanze.sanita.fse2.ms.edsclient.config.Constants;
+import it.finanze.sanita.fse2.ms.edsclient.enums.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ComponentScan(basePackages = {Constants.ComponentScan.BASE})
+@ActiveProfiles(Constants.Profile.TEST)
+class EnumsTest {
+
+    @Test
+    @DisplayName("ProcessOperationEnum test")
+    void processorOperationEnumTest() {
+        String name = ProcessorOperationEnum.CREATE.getName();
+        Assertions.assertEquals(name, ProcessorOperationEnum.CREATE.getName());
+    }
+
+    @Test
+    @DisplayName("AttivitaClinicaEnum test")
+    void attivitaClinicaEnumTest() {
+        String code = "CON";
+        String description = "Consulto";
+        Assertions.assertEquals(code, AttivitaClinicaEnum.CON.getCode());
+        Assertions.assertEquals(description, AttivitaClinicaEnum.CON.getDescription());
+    }
+
+    @Test
+    @DisplayName("AttivitaClinicaEnum test")
+    void practiceSettingCodeEnumTest() {
+        String description = "Allergologia";
+        Assertions.assertEquals(description, PracticeSettingCodeEnum.AD_PSC001.getDescription());
+    }
+
+    @Test
+    @DisplayName("tipoDocLivAltoEnumTest test")
+    void tipoDocLivAltoEnumTest() {
+        String code = TipoDocAltoLivEnum.CER.getCode();
+        String description = TipoDocAltoLivEnum.CER.getDescription();
+        Assertions.assertEquals(code, TipoDocAltoLivEnum.CER.getCode());
+        Assertions.assertEquals(description, TipoDocAltoLivEnum.CER.getDescription());
+    }
+
+    @Test
+    @DisplayName("healthcareFacilityEnumTest test")
+    void healthcareFacilityEnumTest() {
+        String code = HealthcareFacilityEnum.Cittadino.getCode();
+        Assertions.assertEquals(code, HealthcareFacilityEnum.Cittadino.getCode());
+    }
+}
