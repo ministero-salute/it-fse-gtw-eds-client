1d3fcd7def6edbd4d1b1238c7a9f8f98a6f5d683
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2025-04-19 19:35:37 +0200

    feat: Add otel autoinstrument

--- src/main/java/it/finanze/sanita/fse2/ms/edsclient/controller/impl/AbstractCTL.java
+++ src/main/java/it/finanze/sanita/fse2/ms/edsclient/controller/impl/AbstractCTL.java
@@ -11,9 +11,12 @@
  */
 package it.finanze.sanita.fse2.ms.edsclient.controller.impl;
 
+import static it.finanze.sanita.fse2.ms.edsclient.config.Constants.Properties.MS_NAME;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
-import io.micrometer.tracing.Tracer;
+import io.opentelemetry.api.trace.SpanBuilder;
+import io.opentelemetry.api.trace.Tracer;
 import it.finanze.sanita.fse2.ms.edsclient.dto.response.LogTraceInfoDTO;
 
 /**
@@ -27,10 +30,12 @@ public abstract class AbstractCTL{
 
 	protected LogTraceInfoDTO getLogTraceInfo() {
 		LogTraceInfoDTO out = new LogTraceInfoDTO(null, null);
-		if (tracer.currentSpan() != null) {
+		SpanBuilder spanbuilder = tracer.spanBuilder(MS_NAME);
+		
+		if (spanbuilder != null) {
 			out = new LogTraceInfoDTO(
-					tracer.currentSpan().context().spanId(), 
-					tracer.currentSpan().context().traceId());
+					spanbuilder.startSpan().getSpanContext().getSpanId(), 
+					spanbuilder.startSpan().getSpanContext().getTraceId());
 		}
 		return out;
 	}
